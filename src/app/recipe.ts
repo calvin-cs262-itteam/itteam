8f523f7563a37faa6a29090a823db7eab0456662
Josh Wright <wright.jjw@gmail.com>
2019-10-02 12:17:46 -0400

    Recipe constructor

--- src/app/recipe.ts
+++ src/app/recipe.ts
@@ -7,4 +7,17 @@ export class Recipe {
     rating : number;        // Rating
     nratings : number;      // The total number of ratings (used to calculate average)
     //TODO: add tags
+    //TODO: We'll probably want a rating class in the future so we can tie specific ratings to users, but for now we can use numbers
+
+    /**
+     * The constructor doesn't get rating info because ratings start unrated.
+     * @param name Name of the recipe.
+     * @param desc Description of the recipe.
+     */
+    constructor(name:string, desc:string) {
+        this.name = name;
+        this.description = desc;
+        this.rating = 0;
+        this.nratings = 0;
+    }
 }

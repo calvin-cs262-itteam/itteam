c390b14df33c53a57b1335dc8ad7a881bfb9f103
Josh Wright <wright.jjw@gmail.com>
2019-10-02 12:11:45 -0400

    name, description, rating, and nratings fields

--- src/app/recipe.ts
+++ src/app/recipe.ts
@@ -1,2 +1,10 @@
+/**
+ * Recipe class.
+ */
 export class Recipe {
+    name : string;          // The name of the recipe
+    description : string;   // a description of the recipe
+    rating : number;        // Rating
+    nratings : number;      // The total number of ratings (used to calculate average)
+    //TODO: add tags
 }

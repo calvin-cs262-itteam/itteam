59d035c810727b040d42da4fc0ad4c7ca13160e8
Josh Wright <wright.jjw@gmail.com>
2019-10-02 21:59:09 -0400

    Recipe tests

--- src/app/recipe.spec.ts
+++ src/app/recipe.spec.ts
@@ -2,6 +2,21 @@ import { Recipe } from './recipe';
 
 describe('Recipe', () => {
   it('should create an instance', () => {
-    expect(new Recipe()).toBeTruthy();
+    let r = new Recipe("Bacon", "A simple recipe for a simple food", "./fake/dir/img.png");
+    expect(r).toBeTruthy();
+    expect(r.name).toEqual("Bacon");
+    expect(r.description).toEqual("A simple recipe for a simple food");
+    expect(r.rating).toEqual(0);
+    expect(r.nratings).toEqual(0);
+  });
+  it('should take ratings', () => {
+    let r = new Recipe("Bacon", "A simple recipe for a simple food", "./fake/dir/img.png");
+    r.rate(5);
+    expect(r.rating).toEqual(5);
+    expect(r.nratings).toEqual(1);
+
+    r.rate(3);
+    expect(r.rating).toEqual(4);
+    expect(r.nratings).toEqual(2);
   });
 });

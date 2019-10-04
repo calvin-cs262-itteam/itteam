19f91497ac898a75b7e2ebda86dc7109ced5e2ad
Josh Wright <wright.jjw@gmail.com>
2019-10-04 15:53:32 -0400

    Recipe._rating_list

--- src/app/recipe.ts
+++ src/app/recipe.ts
@@ -4,10 +4,10 @@
 export class Recipe {
     private _name : string;          // The name of the recipe
     private _description : string;   // a description of the recipe
-    private _rating : number;        // Rating (average)
+    private _rating : number;        // Rating
     private _rating_list : number[]; // List of ratings
     private _nratings : number;      // The total number of ratings (used to calculate average)
-    private _imgPath : string;       // Path to image of recipe`
+    private _imgPath : string;       // Path to image of recipe
     //TODO: add tags
     //TODO: We'll probably want a rating class in the future so we can tie specific ratings to users, but for now we can use numbers
 
@@ -39,12 +39,11 @@ export class Recipe {
      * @param rating The new rating to factor in.
      */
     rate(rating:number) {
-        this._rating_list.push(rating);
+        this._nratings = this._rating_list.push(rating);
         let total = 0;
         this._rating_list.forEach(r => {
             total += r;     // potential speedup could be storing total instead of calculating every time
         });
-        this._nratings++;
         this._rating = total / this._nratings;
     }
 }

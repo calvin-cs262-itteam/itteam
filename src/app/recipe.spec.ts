479d989010c681694d74bf04708951650feb7a39
Josh Wright <wright.jjw@gmail.com>
2019-10-08 18:56:02 -0400

    new constructor tests

--- src/app/recipe.spec.ts
+++ src/app/recipe.spec.ts
@@ -2,15 +2,17 @@ import { Recipe } from './recipe';
 
 describe('Recipe', () => {
   it('should create an instance', () => {
-    let r = new Recipe("Bacon", "A simple recipe for a simple food", "./fake/dir/img.png");
+    let r = new Recipe("Bacon", "A simple recipe for a simple food", "./fake/dir/img.png", ["Get bacon", "Cook bacon"], ["Bacon"]);
     expect(r).toBeTruthy();
     expect(r.name).toEqual("Bacon");
     expect(r.description).toEqual("A simple recipe for a simple food");
     expect(r.rating).toEqual(0);
     expect(r.nratings).toEqual(0);
+    expect(r.instructions).toEqual(["Get bacon", "Cook bacon"]);
+    expect(r.ingredients).toEqual(["Bacon"]);
   });
   it('should take ratings', () => {
-    let r = new Recipe("Bacon", "A simple recipe for a simple food", "./fake/dir/img.png");
+    let r = new Recipe("Bacon", "A simple recipe for a simple food", "./fake/dir/img.png", ["Get bacon", "Cook bacon"], ["Bacon"]);
     r.rate(5);
     expect(r.rating).toEqual(5);
     expect(r.nratings).toEqual(1);

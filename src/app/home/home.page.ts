7596d2e5916daa1cca3e5fe7b89b70fed1b90ab5
unknown <njw32@students.calvin.edu>
2019-10-09 21:39:22 -0400

    add 4 recipes to homepage

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -61,7 +61,7 @@ export class HomePage {
     this.instant_ramen = new Recipe(
       "Instant Ramen for Adults",
       "You really can't mess this one up. Serves 1 or 2, takes 8 minutes to make.",
-      this.IMG_PATH + "instant_ramen.jpg",
+      this.IMG_PATH + "Instant Ramen.jpg",
       ["1) Bring 2 cups of water to a boil in a small pot over high heat. Reduce the heat to medium and add the ramen noodles, frozen veggies, turmeric, 1/2 teaspoon of the ramen seasoning, and 1/2 teaspoon salt; stir to combine. Return the water to a boil and cook until the veggies are crisp-tender with some bite, about 3 minutes.",
       "2) Add the chicken and cook until just warmed through, about 1 minute. Remove from the heat and ladle the vegetables, chicken, and noodles into a bowl. Pour the broth over the top and serve."],
       ["1 (3-ounce) package instant ramen (any flavor), seasoning packet reserved", 

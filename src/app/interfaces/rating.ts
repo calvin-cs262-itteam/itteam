4b9a0d6a4870bea0e9c0b0a62a4616157a467ee6
Josh Wright <wright.jjw@gmail.com>
2019-10-16 17:41:01 -0400

    rating score, userid, and comment fields

--- src/app/interfaces/rating.ts
+++ src/app/interfaces/rating.ts
@@ -1,2 +1,8 @@
+/**
+ * Rating class.
+ */
 export class Rating {
+    private _score : number;    // The rating.
+    private _userid : number;   // The userID of the rater.
+    private _comment : string;  // Rating comment.
 }

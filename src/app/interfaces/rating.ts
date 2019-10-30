19f698d79873b7e2b473caec94f0a50840cc5d13
Josh Wright <wright.jjw@gmail.com>
2019-10-30 18:17:43 -0400

    rating id

--- src/app/interfaces/rating.ts
+++ src/app/interfaces/rating.ts
@@ -2,10 +2,12 @@
  * Rating class.
  */
 export class Rating {
+    private _id : number;       // The rating ID
     private _score : number;    // The rating.
     private _userid : number;   // The userID of the rater.
     private _comment : string;  // Rating comment.
 
+    get id() : number {return this._id}
     get score() : number {return this._score}
     get userid() : number {return this._userid}
     get comment() : string {return this._comment}

187c8fd062d13a25fe9046c870d617a704a7c599
Ian Lilienthal <ianlilienthal@Ians-MacBook-Pro.local>
2019-12-09 20:43:14 -0500

    Add Recipe Page mk1

--- src/app/add-recipe/add-recipe.module.ts
+++ src/app/add-recipe/add-recipe.module.ts
@@ -1,25 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { Routes, RouterModule } from '@angular/router';
-
 import { IonicModule } from '@ionic/angular';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AddRecipePage } from './add-recipe.page';
-
-const routes: Routes = [
-  {
-    path: '',
-    component: AddRecipePage
-  }
-];
 
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule,
+    ReactiveFormsModule,
     IonicModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild([
+      {
+        path: '',
+        component: AddRecipePage
+      }
+    ])
   ],
   declarations: [AddRecipePage]
 })

a9cef0f437c0a510f18a153c83ac5cf771c37320
agj4 <fishyanni@gmail.com>
2019-12-10 22:03:42 -0500

    added documentation

--- src/app/popover/popover.component.ts
+++ src/app/popover/popover.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { PopoverController, NavParams } from '@ionic/angular';
 import { Instruction } from '../instruction';
 
+/* 
+ * Popover component for suggestions
+ */
 @Component({
   selector: 'app-popover',
   templateUrl: './popover.component.html',
@@ -12,10 +15,19 @@ export class PopoverComponent implements OnInit {
   instr : Instruction;
   givenSug : string
 
-  constructor(private popConp : PopoverController, public navParams: NavParams) {
+  /**
+   * Popover controller constructor
+   * @param popCont the popover controller
+   * @param navParams given parameters, 
+   *     including instr the instruction calling popover
+   */
+  constructor(private popCont : PopoverController, public navParams: NavParams) {
     this.instr = this.navParams.get('instr')
    }
 
+   /* adds the suggestion in input to instruction
+    * requirement: givenSug must not be empty
+    */
    addSug() {
      if (this.givenSug != "" && this.givenSug){
       this.instr.addSuggestion(this.givenSug);
